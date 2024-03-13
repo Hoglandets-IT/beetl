bd153dcb4632bb41433f0958858310f3366149b7
Lars <lars.scheibling@hoglandet.se>
2024-03-13 13:32:04 +0100

    Fix version for CI

--- src/beetl/__version__.py
+++ src/beetl/__version__.py
@@ -11,6 +11,9 @@ __version__ = os.getenv("GHRELEASE", "0.4.1")
 if os.getenv('GHRUN', False) and os.getenv('GHBRANCH', 'develop') == 'develop':
     __version__ += f".{os.getenv('RUN_ID')}"
 
+if __version__ == "":
+    __version__ = "0.4.1"
+
 __author__ = "Lars Scheibling"
 __author_email__ = "lars.scheibling@hoglandet.se"
 __license__ = "GnuPG 3.0"
