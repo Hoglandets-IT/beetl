dcf65a34fded4de13250062ab9425756cd6e34ad
Zackarias Montell <zackarias.montell@hoglandet.se>
2024-11-19 08:04:47 +0100

    Add automatic accept of the eula

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,8 +1,8 @@
+#!/bin/sh
 curl https://packages.microsoft.com/keys/microsoft.asc | sudo tee /etc/apt/trusted.gpg.d/microsoft.asc \
     && curl https://packages.microsoft.com/config/debian/11/prod.list | sudo tee /etc/apt/sources.list.d/mssql-release.list \
     && sudo apt-get update \
-    && export ACCEPT_EULA=Y \
-    && sudo apt-get -y install libmariadb-dev libpq-dev unixodbc unixodbc-dev python3-venv msodbcsql18 libgssapi-krb5-2 \
+    && sudo ACCEPT_EULA=Y apt-get -y install libmariadb-dev libpq-dev unixodbc unixodbc-dev python3-venv msodbcsql18 libgssapi-krb5-2 \
     && sudo apt-get clean \
     && pip3 install --user -r requirements.txt \
     && pip3 install --user -r doc-requirements.txt
