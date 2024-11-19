1e0cc550fbd9783db8bf99a9587fc3fda1ec0f9b
Zackarias Montell <zackarias.montell@hoglandet.se>
2024-11-19 10:16:10 +0100

    Removed old docs requirements file

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-curl https://packages.microsoft.com/keys/microsoft.asc | sudo tee /etc/apt/trusted.gpg.d/microsoft.asc \
+
+mkdir /tmp/beetl && \
+    && curl https://packages.microsoft.com/keys/microsoft.asc | sudo tee /etc/apt/trusted.gpg.d/microsoft.asc \
     && curl https://packages.microsoft.com/config/debian/11/prod.list | sudo tee /etc/apt/sources.list.d/mssql-release.list \
     && sudo apt-get update \
     && sudo ACCEPT_EULA=Y apt-get -y install libmariadb-dev libpq-dev unixodbc unixodbc-dev python3-venv msodbcsql18 libgssapi-krb5-2 \
     && sudo apt-get clean \
-    && pip3 install --user -r requirements.txt \
-    && pip3 install --user -r doc-requirements.txt
+    && pip3 install --user -r requirements.txt
--- tests.py
+++ tests.py
@@ -1,26 +1,26 @@
-# import unittest
-# from tests.test_functions import *
+import unittest
+from tests.test_functions import *
 
-# if __name__ == '__main__':
-#     unittest.main()
-# import polars as pl
-
-
-# data = pl.DataFrame({
-#     "id": [7, 5, 6],
-#     "name": ["John", "Jane", "Alice"],
-#     "email": ["john@example.com", "jane@example.com", "alice@example.com"]
-# })
+if __name__ == '__main__':
+    unittest.main()
+import polars as pl
 
 
-# data.write_database(
-#     "dsttable",
-#     "mysql+pymysql://root@localhost:3306/testdb",
-#     if_exists="append"
-# )
+data = pl.DataFrame({
+    "id": [7, 5, 6],
+    "name": ["John", "Jane", "Alice"],
+    "email": ["john@example.com", "jane@example.com", "alice@example.com"]
+})
 
-from src.beetl.beetl import Beetl, BeetlConfig
 
-conf = BeetlConfig.from_yaml_file('localtest_binary.yaml')
-sync = Beetl(conf)
-sync.sync()
+data.write_database(
+    "dsttable",
+    "mysql+pymysql://root@localhost:3306/testdb",
+    if_exists="append"
+)
+
+# from src.beetl.beetl import Beetl, BeetlConfig
+
+# conf = BeetlConfig.from_yaml_file('localtest_binary.yaml')
+# sync = Beetl(conf)
+# sync.sync()
