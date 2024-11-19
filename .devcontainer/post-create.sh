0183d396fcd82f686d34846c137421390baa0bce
Zackarias Montell <izacer119@gmail.com>
2024-11-19 10:36:46 +0100

    Devcontainer for local development (#9)
    * Added devcontainer and task to run docs
    
    * Adjusted devcontainer to run beetl
    
    * Fixed issue with invisible character in script
    
    * Add automatic accept of the eula
    
    * Removed old docs requirements file
    
    ---------
    
    Co-authored-by: Zackarias Montell <zackarias.montell@hoglandet.se>

---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+mkdir /tmp/beetl && \
+    && curl https://packages.microsoft.com/keys/microsoft.asc | sudo tee /etc/apt/trusted.gpg.d/microsoft.asc \
+    && curl https://packages.microsoft.com/config/debian/11/prod.list | sudo tee /etc/apt/sources.list.d/mssql-release.list \
+    && sudo apt-get update \
+    && sudo ACCEPT_EULA=Y apt-get -y install libmariadb-dev libpq-dev unixodbc unixodbc-dev python3-venv msodbcsql18 libgssapi-krb5-2 \
+    && sudo apt-get clean \
+    && pip3 install --user -r requirements.txt
