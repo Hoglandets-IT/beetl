a5fbc1bc77250163426e837af2078d593b22307b
wholteza <zackarias@montell.se>
2024-11-27 15:25:40 +0000

    Add test between mongodb and relational database

--- tests/configurations/mongodb.py
+++ tests/configurations/mongodb.py
@@ -136,3 +136,121 @@ def to_mongodb_with_int_as_identifier(connectionString: str):
             }
         ]
     }
+
+
+def to_mysql_with_object_id_as_identifier(mongodb_connection_string: str, mysql_connection_string: str):
+    if (not mongodb_connection_string):
+        raise Exception("Connection string is required")
+
+    return {
+        "version": "V1",
+        "sources": [
+            {
+                "name": "srcdb",
+                "type": "Mongodb",
+                "connection": {
+                    "settings": {
+                        "connection_string": mongodb_connection_string,
+                        "database": "test",
+                    }
+
+                }
+            },
+            {
+                "name": "dstdb",
+                "type": "Mysql",
+                "connection": {
+                    "settings": {
+                        "connection_string": mysql_connection_string,
+                    }
+
+                }
+            }
+
+        ],
+        "sync": [
+            {
+                "source": "srcdb",
+                "destination": "dstdb",
+                "sourceConfig": {
+                    "collection": "src",
+                    "unique_columns": ["_id"],
+                    "comparison_columns": ["_id", "email"],
+                    "columns": [
+                        {
+                            "name": "id",
+                            "type": "Utf8",
+                            "unique": True,
+                        },
+                        {
+                            "name": "email",
+                            "type": "Utf8",
+                            "unique": False,
+                        }
+                    ]
+                },
+                "destinationConfig": {
+                    "table": "dst",
+                    "columns": [
+                        {
+                            "name": "id",
+                            "type": "Utf8",
+                            "unique": True,
+                        },
+                        {
+                            "name": "email",
+                            "type": "Utf8",
+                            "unique": False,
+                        }
+                    ]
+                },
+                "sourceTransformers": [
+                    {
+                        "transformer": "frames.rename_columns",
+                        "config": {
+                            "columns": [
+                                {"from": "_id", "to": "id"}
+                            ]
+                        }
+                    },
+                    {
+                        "transformer": "structs.jsonpath",
+                        "config": {
+                            "jsonPath": "$.*.name",
+                            "inField": "children",
+                            "outField": "children_concatinated",
+                        }
+                    },
+                    {
+                        "transformer": "frames.drop_columns",
+                        "config": {
+                            "columns": ["children"]
+                        }
+                    },
+                    {
+                        "transformer": "strings.join_listfield",
+                        "config": {
+                            "inField": "children_concatinated",
+                            "outField": "children",
+                            "separator": ", "
+                        }
+                    },
+                    {
+                        "transformer": "structs.jsonpath",
+                        "config": {
+                            "jsonPath": "$.city",
+                            "inField": "address",
+                            "outField": "city",
+                        }
+                    },
+                    {
+                        "transformer": "frames.project_columns",
+                        "config": {
+                            "columns": ["id", "name", "email", "city", "children"]
+                        }
+                    }
+                ]
+
+            }
+        ]
+    }
--- tests/functionality_testing/beetl/sources/test_mongodb.py
+++ tests/functionality_testing/beetl/sources/test_mongodb.py
@@ -1,11 +1,15 @@
 import unittest
 
 from bson import ObjectId
+import sqlalchemy
 from src.beetl import beetl
 from testcontainers.mongodb import MongoDbContainer
-from tests.configurations.mongodb import to_mongodb_with_object_id_as_identifier, to_mongodb_with_int_as_identifier
+from testcontainers.mysql import MySqlContainer
+from tests.configurations.mongodb import to_mongodb_with_object_id_as_identifier, to_mongodb_with_int_as_identifier, to_mysql_with_object_id_as_identifier
 from tests.helpers.manual_result import ManualResult
 from faker import Faker
+
+from tests.helpers.mysql_testcontainer import to_connection_string
 
 DATABASE_NAME = "test"
 SOURCE_TABLE_NAME = "src"
@@ -46,6 +50,9 @@ class TestMongodbSource(unittest.TestCase):
             self.assertEqual(result.deleted_count, 1)
 
     def test_sync_between_two_mongodb_sources_with_object_id(self):
+        """
+        This test requires transformers to ensure that the object id field is transformed back to an object id on insertion since the mongodb source is automatically converting it to a string on querying.
+        """
         with MongoDbContainer() as mongodb:
             # Arrange
             inserted_ids = self.insert_test_data(3, "object_id", mongodb)
@@ -108,3 +115,68 @@ class TestMongodbSource(unittest.TestCase):
 
             oneRecordWasDeleted = ManualResult(0, 0, 1)
             self.assertEqual(deleteResult, oneRecordWasDeleted)
+
+    def test_sync_between_mongodb_and_mysql(self):
+        """
+        This test is to ensure that data can be synced between MongoDB and a SQL database. Syncing between a document store and a relational database can be challenging due to the differences in data structure. This test will ensure that the beetl library can handle these differences and sync data between the two sources.
+
+        The MongoDB source will be configured with documents looking like this:
+        {
+            "_id": ObjectId(),
+            "name": "John Doe",
+            "email": "john@doe.com",
+            "children": [{"name": "Jane Doe"}, {"name": "Joseph Doe"}],
+            "address": {"city": "New York", "state": "NY"}
+        }
+
+        The MySQL source will be configured with a table looking like this:
+
+        +----+----------+-----------------+-----------------+----------------------+
+        | id | name     | email           | city            | children             |
+        +----+----------+-----------------+-----------------+----------------------+
+        | 1  | John Doe | john@doe.com    | New York        | Jane Doe, Joseph Doe |
+
+        There are a few things to note here:
+        - The source has a nested document from which we want to extract the city field, this requires a jsonpath transformer to extract the city field from the address field in the source.
+        - The sources _id is an ObjectId, which is not supported by MySQL, the mongodb source will transform this to a string but we need to use the rename transformer to match the column name in the MySQL source
+        - The children field is an array of documents, we want to extract the name field from each document and concatenate them into a single string separated by a comma. This requires a jsonpath transformer to extract the name field from each child document and a string join transformer to concatenate the names into a single string.
+
+        """
+        with MongoDbContainer() as mongodb:
+            with MySqlContainer("mysql:latest", ) as mysql:
+                # Arrange
+                engine = sqlalchemy.create_engine(mysql.get_connection_url())
+                with engine.begin() as connection:
+                    command = f"""
+                        create table dst (id varchar({len(str(ObjectId()))}) primary key, name varchar(255), email varchar(255), city varchar(255), children varchar(255));"""
+                    connection.execute(sqlalchemy.text(command))
+
+                inserted_ids = self.insert_test_data(3, "object_id", mongodb)
+                config = to_mysql_with_object_id_as_identifier(
+                    mongodb.get_connection_url(), to_connection_string(mysql.get_connection_url()))
+                beetlInstance = beetl.Beetl(beetl.BeetlConfig(config))
+
+                # Act
+                createResult = beetlInstance.sync()
+
+                noActionResult = beetlInstance.sync()
+
+                self.update_test_data(
+                    inserted_ids[0], self.faker.email(), mongodb)
+                updateResult = beetlInstance.sync()
+
+                self.delete_test_data(inserted_ids[0], mongodb)
+                deleteResult = beetlInstance.sync()
+
+                # Assert
+                allEntriesWereSynced = ManualResult(3, 0, 0)
+                self.assertEqual(createResult, allEntriesWereSynced)
+
+                nothingChanged = ManualResult(0, 0, 0)
+                self.assertEqual(noActionResult, nothingChanged)
+
+                oneRecordWasUpdated = ManualResult(0, 1, 0)
+                self.assertEqual(updateResult, oneRecordWasUpdated)
+
+                oneRecordWasDeleted = ManualResult(0, 0, 1)
+                self.assertEqual(deleteResult, oneRecordWasDeleted)
