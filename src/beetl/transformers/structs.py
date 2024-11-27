eeac81be82ffe6c41e70ef726be607cab3425ccb
wholteza <zackarias@montell.se>
2024-11-27 15:25:15 +0000

    Add support for * selector for lists in jsonpath transformer

--- src/beetl/transformers/structs.py
+++ src/beetl/transformers/structs.py
@@ -1,7 +1,8 @@
-from typing import List
+from typing import Any, List
 import polars as pl
 from .interface import TransformerInterface, register_transformer_class
 import json
+
 
 @register_transformer_class("structs")
 class StructTransformers(TransformerInterface):
@@ -24,7 +25,7 @@ class StructTransformers(TransformerInterface):
     def jsonpath(data: pl.DataFrame, inField: str, outField: str, jsonPath: str, defaultValue: str = ""):
         """
         Retrieve a value from a jsonpath inside of a column and add it to a new column
-        
+
         Args:
             data (pl.DataFrame): The dataFrame to modify
             field (str): The field to extract the jsonpath from
@@ -32,27 +33,36 @@ class StructTransformers(TransformerInterface):
             jsonPath (str): The jsonpath to extract
         """
         split_jsonpath = jsonPath.split(".")
-                
-        def jsonpathFunction(fData):
+
+        def get_value_at_jsonpath(fData: Any, start_at: int = 0):
             if isinstance(fData, str):
                 fData = json.loads(fData)
 
             try:
-                for i in split_jsonpath:
+                for index, selector in enumerate(split_jsonpath[start_at:], start=start_at):
+                    if selector == "$":
+                        continue
+                    if selector == "*":
+                        if not isinstance(fData, list) and not isinstance(fData, pl.Series):
+                            raise NotImplementedError(
+                                "Wildcard selector is only supported for lists")
+                        fData = [get_value_at_jsonpath(fData[j], index + 1)
+                                 for j in range(len(fData))]
+                        return fData
                     try:
-                        ix = int(i)
+                        ix = int(selector)
                         fData = fData[ix]
                     except:
-                        fData = fData[i]
+                        fData = fData[selector]
                 return fData
             except:
                 return defaultValue
-                    
+
         dCol = data[inField]
         outCol = []
         for i in range(len(dCol)):
-            outCol.append(jsonpathFunction(dCol[i]))
-        
+            outCol.append(get_value_at_jsonpath(dCol[i]))
+
         data = data.with_columns(pl.Series(outField, outCol))
-        
+
         return data
