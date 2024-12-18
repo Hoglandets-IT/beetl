1ee0662c5f3e83b7c1895a1fbf94bd49701a59b1
Zackarias Montell <zackarias@montell.se>
2024-12-18 11:29:08 +0100

    Add sanity test of installed beetl package (#36)

---
+++ tests/distribution_testing/__init__.py
@@ -0,0 +1,3 @@
+"""These tests run syncs using the installed version of beetl, not using the source files.
+
+This is to ensure that the distribution is working correctly after creating a build using `python -m build`."""
