de04ee4c6903fb169bda0bc825450753b637b50e
Zackarias Montell <zackarias@montell.se>
2024-12-26 10:49:42 +0100

    Install psycopg according to readme (#46)
    - Fix bug where multiple invocations of `Beetl.sync` run after each other aggregated the number of syncs instead of creating separate results.
    - Started using connect and disconnect on sources before and after syncing to enable atomic operations for the sources that support it.
    - Fixed bug where the transformer int.fillna did not fill nulls.
    - Defined psycopg dependency as per their documentation.

---
+++ tests/unit_testing/transformers/test_integer.py
@@ -0,0 +1,56 @@
+
+import unittest
+
+from polars import DataFrame, Series, Float64, Utf8
+
+from src.beetl.transformers.integer import IntegerTransformer
+
+
+class UnitTestIntegerTransformers(unittest.TestCase):
+
+    def test_fillna__integer_field_with_nulls__nulls_are_filled_with_value(self):
+        # arrange
+        inField = "field1"
+        value = 1
+        data = DataFrame().with_columns(Series(inField, [None, None, value]))
+        transformer = IntegerTransformer()
+
+        # act
+        result = transformer.fillna(data, inField, value=value)
+
+        # assert
+        self.assertEqual([value, value, value],
+                         result[inField].to_list())
+
+    def test_fillna__nan_supported_field_type_with_nans__nans_are_filled_with_value(self):
+        # arrange
+        inField = "field1"
+        value = 1
+        nan = float("nan")
+        data = DataFrame().with_columns(
+            Series(inField, [nan, nan, value], dtype=Float64))
+        transformer = IntegerTransformer()
+
+        # act
+        result = transformer.fillna(data, inField, value=value)
+
+        # assert
+        self.assertEqual([value, value, value],
+                         result[inField].to_list())
+
+    def test_fillna__not_nan_supported_field_type_with_nans__nans_are_not_filled_with_value(self):
+        # arrange
+        inField = "field1"
+        value = "value"
+        nan_value = float("nan")
+        data = DataFrame().with_columns(
+            Series(inField, [nan_value, nan_value, value], dtype=Utf8, strict=False))
+        transformer = IntegerTransformer()
+
+        # act
+        result = transformer.fillna(data, inField, value=value)
+
+        # assert
+        nan_string = 'NaN'
+        self.assertEqual([nan_string, nan_string, value],
+                         result[inField].to_list())
