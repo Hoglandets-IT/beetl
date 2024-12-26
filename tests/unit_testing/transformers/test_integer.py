7be05b03993d15d958f85c56e1d01174153c9e6c
wholteza <zackarias@montell.se>
2024-12-26 09:09:49 +0000

    Tested int.fillna

---
+++ tests/unit_testing/transformers/test_integer.py
@@ -0,0 +1,55 @@
+
+import unittest
+
+from polars import DataFrame, Series, Float64, Utf8
+
+from src.beetl.transformers.integer import IntegerTransformer
+
+
+class UnitTestIntegerTransformers(unittest.TestCase):
+
+    def test_fillna__integer_field_with_nulls__nulls_are_filled_with_value(self):
+        # arrange
+        inField = "field1"
+        value = 1
+        data = DataFrame().with_columns(Series(inField, [None, None, value]))
+        transformer = IntegerTransformer()
+
+        # act
+        result = transformer.fillna(data, inField, value=value)
+
+        # assert
+        self.assertEqual([value, value, value],
+                         result[inField].to_list())
+
+    def test_fillna__integer_field_with_nans_and_is_nan_supported_type__nans_are_filled_with_value(self):
+        # arrange
+        inField = "field1"
+        value = 1
+        nan = float("nan")
+        data = DataFrame().with_columns(
+            Series(inField, [nan, nan, value], dtype=Float64))
+        transformer = IntegerTransformer()
+
+        # act
+        result = transformer.fillna(data, inField, value=value)
+
+        # assert
+        self.assertEqual([value, value, value],
+                         result[inField].to_list())
+
+    def test_fillna__integer_field_with_nans_and_is_not_nan_supported_type__nans_are_not_filled_with_value(self):
+        # arrange
+        inField = "field1"
+        value = "value"
+        nan = float("nan")
+        data = DataFrame().with_columns(
+            Series(inField, [nan, nan, value], dtype=Utf8, strict=False))
+        transformer = IntegerTransformer()
+
+        # act
+        result = transformer.fillna(data, inField, value=value)
+
+        # assert
+        self.assertEqual([nan, nan, value],
+                         result[inField].to_list())
