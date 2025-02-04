2c362e19d855a6c92a937b2c9e98d76d8d484eb3
wholteza <zackarias@montell.se>
2025-02-04 15:19:43 +0000

    Adjusted structs.staticfield schema to allow any static value

--- src/beetl/transformers/structs_schema.py
+++ src/beetl/transformers/structs_schema.py
@@ -1,4 +1,4 @@
-from typing import Annotated, Literal, Optional, Union
+from typing import Annotated, Any, Literal, Optional, Union
 
 from pydantic import Field
 
@@ -9,7 +9,7 @@ class StructTransformerSchema:
     class StaticField(TransformerSchemaBase):
         class Config(TransformerConfigBase):
             field: Annotated[str, Field(min_length=1)]
-            value: str
+            value: Any
             only_add_if_missing: Optional[bool] = False
 
         transformer: Literal["structs.staticfield"]
