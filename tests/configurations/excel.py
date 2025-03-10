955a5b0541fa2f6f5af49118592e2a649d1c5929
Lars Scheibling <24367830+scheibling@users.noreply.github.com>
2025-03-10 14:52:36 +0100

    Add excel data import (#73)
    * Add excel data import
    
    * Added tests for Excel

---
+++ tests/configurations/excel.py
@@ -0,0 +1,40 @@
+def to_xlsx(source_file_path: str, destination_file_path: str) -> dict:
+    return {
+        "version": "V1",
+        "sources": [
+            {
+                "name": "src",
+                "type": "Excel",
+                "connection": {
+                    "path": source_file_path,
+                },
+            },
+            {
+                "name": "dst",
+                "type": "Excel",
+                "connection": {
+                    "path": destination_file_path,
+                },
+            },
+        ],
+        "sync": [
+            {
+                "dryRun": True,
+                "source": "src",
+                "destination": "dst",
+                "sourceConfig": {},
+                "destinationConfig": {},
+                "comparisonColumns": {
+                    "ID": "Int32",
+                    "Postcode": "Utf8",
+                    "Sales_Rep_ID": "Utf8",
+                    "Sales_Rep_Name": "Utf8",
+                    "Year": "Utf8",
+                    "Value": "Utf8",
+                },
+                "sourceTransformers": [],
+                "destinationTransformers": [],
+                "insertionTransformers": [],
+            }
+        ],
+    }
