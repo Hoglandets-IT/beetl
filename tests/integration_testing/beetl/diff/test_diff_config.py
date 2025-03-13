7d0a8cc32e021ee46a490b8c4a3dc637d438b791
wholteza <zackarias@montell.se>
2025-03-13 12:17:30 +0000

    Added diff test config

--- tests/integration_testing/beetl/diff/test_diff_config.py
+++ tests/integration_testing/beetl/diff/test_diff_config.py
@@ -5,5 +5,39 @@ from src.beetl.config.config_base import BeetlConfig
 
 class DiffIntegrationTests(TestCase):
     def test_diff_config(self):
-        dict_config = {}
-        config = BeetlConfig(config)
+        dict_config = {
+            "version": "V1",
+            "sources": [
+                {
+                    "name": "src",
+                    "type": "Static",
+                    "connection": {
+                        "static": [
+                            {"id": 1, "name": "test1"},
+                            {"id": 2, "name": "test2"},
+                            {"id": 3, "name": "test3"},
+                            {"id": 4, "name": "test4"},
+                        ]
+                    },
+                },
+                {
+                    "name": "dst",
+                    "type": "Static",
+                    "connection": {"static": []},
+                },
+            ],
+            "sync": [
+                {
+                    "name": "diff test",
+                    "source": "src",
+                    "destination": "dst",
+                    "sourceConfig": {},
+                    "destinationConfig": {},
+                    "comparisonColumns": [
+                        {"name": "id", "type": "Int64", "unique": True}
+                    ],
+                    "diff": {"type": "Static", "name": "dst", "config": {}},
+                }
+            ],
+        }
+        config = BeetlConfig(dict_config)
