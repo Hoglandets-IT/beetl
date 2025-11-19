4a5e90edb518416debc18f5e204714d71daed5dd
Zackarias Montell <zackarias.montell@hoglandet.se>
2025-11-19 14:05:36 +0100

    Adding integration test for excel sheet name

--- tests/integration_testing/beetl/sources/test_excel.py
+++ tests/integration_testing/beetl/sources/test_excel.py
@@ -5,13 +5,18 @@ import polars as pl
 
 from src.beetl import beetl
 from src.beetl.sources.excel.excel_source import ExcelSource
-from tests.configurations.excel import diff_to_xlsx, to_xlsx
+from src.beetl.comparison_result import ComparisonResult
+from tests.configurations.excel import diff_to_xlsx, to_xlsx, to_xlsx_with_sheet_name
 from tests.helpers.manual_result import ManualResult
 from tests.helpers.temp import TEMP_PATH, clean_temp_directory
 
 
 class TestExcelSource(unittest.TestCase):
-    """Basic test of the static source found in src/beetl/sources/static.py"""
+    """Basic test of the static source found in src/beetl/sources/static.py
+
+    The tests in where work since dst is never updated.
+    That is because using excel as a destination isn't implemented yet.
+    """
 
     def test_xlsx_loading(self):
         # Arrange
@@ -29,6 +34,27 @@ class TestExcelSource(unittest.TestCase):
 
         # # Assert
         self.assertEqual(amounts, ManualResult(91, 7, 8))
+
+    def test_excel_with_multiple_workbooks__when_workbook_is_specified__loads_that_workbook(
+        self,
+    ):
+        # Arrange
+        betl = beetl.Beetl(
+            beetl.BeetlConfig(
+                to_xlsx_with_sheet_name(
+                    "tests/fake-data/datafiles/src_with_multiple_workbooks.xlsx",
+                    "tests/fake-data/datafiles/dst.xlsx",
+                )
+            )
+        )
+
+        # Act
+        dry_run: list[ComparisonResult] = betl.sync(dry_run=True)
+        changes = dry_run[0]
+        result = changes.to_beetl_result()
+
+        # # Assert
+        self.assertEqual(result, ManualResult(91, 7, 8))
 
     def test_save_diff__when_configured__diff_is_saved_to_temp_dir(self):
         clean_temp_directory()
