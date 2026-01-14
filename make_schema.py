fa83b64e699c4dacbb5b2757fcb6ae96dc339315
Lars Scheibling <24367830+scheibling@users.noreply.github.com>
2026-01-14 14:43:58 +0100

    Add write option for path

--- make_schema.py
+++ make_schema.py
@@ -3,7 +3,7 @@ from src.beetl.config.v1.v1_schema import BeetlConfigSchemaV1
 
 jsonschema = BeetlConfigSchemaV1.model_json_schema()
 
-with open("docs/.vitepress/dist/schema_v1.json") as f:
+with open("docs/.vitepress/dist/schema_v1.json", 'w') as f:
     json.dump(jsonschema, f, indent=2)
 
 print("Wrote schema to docs/.vitepress/dist/schema_v1.json")
